2095963d52cdf7105ff62b58119f99efd08a909e
Nagesh Devadiga <113429048+nageshdevadig-a@users.noreply.github.com>
2025-10-16 12:42:22 +0530

    Initialize Vyuha server project with Gradle setup and basic application structure

---
+++ vyuha-server/src/main/java/com/nageshdevadiga/vyuha/server/VyuhaServerApplication.java
@@ -0,0 +1,13 @@
+package com.nageshdevadiga.vyuha.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyuhaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyuhaServerApplication.class, args);
+	}
+
+}
