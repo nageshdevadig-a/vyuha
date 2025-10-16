07175b2063af48d4ae169a3765b0ca1acd6f157e
nageshdevadiga <nageshdevadiga@zohomail.in>
2025-10-16 19:15:00 +0530

    Implement Java agent with premain method and update build configuration

--- vyuha-agent/src/main/java/com/nageshdevadiga/vyuha/agent/AgentMain.java
+++ vyuha-agent/src/main/java/com/nageshdevadiga/vyuha/agent/AgentMain.java
@@ -1,4 +1,10 @@
 package com.nageshdevadiga.vyuha.agent;
-
+import java.lang.instrument.Instrumentation;
 public class AgentMain {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("Agent starting ....");
+        MetricsCollector collector = new MetricsCollector();
+        collector.setDaemon(true);
+        collector.start();
+    }
 }
