47149d49a0439567e79b4e505ed33348f3238186
nageshdevadiga <nageshdevadiga@zohomail.in>
2025-10-17 17:33:46 +0530

    Refactor AgentMain to initialize MetricsCollector in a separate thread

--- vyuha-agent/src/main/java/com/nageshdevadiga/vyuha/agent/AgentMain.java
+++ vyuha-agent/src/main/java/com/nageshdevadiga/vyuha/agent/AgentMain.java
@@ -2,9 +2,10 @@ package com.nageshdevadiga.vyuha.agent;
 import java.lang.instrument.Instrumentation;
 public class AgentMain {
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("Agent starting ....");
-        MetricsCollector collector = new MetricsCollector();
+        System.out.println("Agent Initialized ....");
+        Thread collector = new Thread(new MetricsCollector());
         collector.setDaemon(true);
         collector.start();
+        System.out.println("Metrics Collector Started ....");
     }
 }
